ca44c38d184cef589fa85863945271f84549b843
190107 <190107@e0601-05.JC-21.local>
2022-05-27 12:16:14 +0900

    課題5

--- src/main/java/jp/te4a/spring/boot/mybootapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/mybootapp5/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 //view使うために@RestControllerを@Controller
 @Controller
-
 public class HelloController {
 	
 	@RequestMapping(value="/post", method=RequestMethod.POST)
@@ -13,13 +12,11 @@ public class HelloController {
 	    ModelAndView mv = new ModelAndView("index");
 	    mv.addObject("msg", "you write '" + text1 + "'!!!");
 	    return mv;
+	}
 
-	    // @RequestMapping("/")
-	    // public String index(Model model) {
-        //model.addAttribute("msg", "this is  a setting message");
-	    //public String index() {
-        //return "this is Spring Boot sample.";
-        //return "index";
+    @RequestMapping("/")
+    public String index(Model model) {
+    	model.addAttribute("msg", "this is  a setting message");
+        return "index";
     }
-
 }
